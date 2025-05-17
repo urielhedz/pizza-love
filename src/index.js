62092102b33510c1e1ce69c4ff24cf37c34b51b2
urielhdz <uriel.hdzc@gmail.com>
2025-05-16 18:22:53 -0600

    agregue un api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import pizzas from '../data/pizzas.json';
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		let urlObject = new URL(request.url);
+
+		if (urlObject.pathname == '/admin') {
+			return new Response('Hola admin');
+		}
+
+		if (urlObject.pathname == '/pizzas') {
+			return new Response(JSON.stringify(pizzas), {
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			});
+		}
+
+		return new Response('Not found', { status: 404 });
 	},
 };
